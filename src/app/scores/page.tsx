65e35435f867c5961841b88355e9a981591763a9
Javier Sayago <javier.sayago000@gmail.com>
2025-12-09 20:57:52 +0000

    Try fixing this error: `Runtime Error: Error: useRouter is not defined.

--- src/app/scores/page.tsx
+++ src/app/scores/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
